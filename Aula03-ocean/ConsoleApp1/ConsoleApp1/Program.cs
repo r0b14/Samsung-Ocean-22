7cb0c773a0dc9ac30c36388b104ce85d057777dd
Robson Thiago <56169778+r0b14@users.noreply.github.com>
2022-04-27 19:24:14 -0300

    projeto 1

---
+++ Aula03-ocean/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args) // é preciso que o programa esteja escrito dentro do main
+        {
+            //usando variaveis
+            string nome = "Robson";
+            /* String -> tipo da variavel 
+             * nome -> nome da variavel
+             * = -> atribuição
+             * "Robson" -> o 'valor' que vai ser guardado no variavel do tipo string
+             */
+            int idade = 22;
+            /**
+             * Comentário de varias linhas
+            **/
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+        }
+    }
+}
